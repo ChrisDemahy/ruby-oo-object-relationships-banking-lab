b3dc99ca1b22852e5b4e5aa4dc1ac80abd93d1fa
ChrisDemahy <demahyc@gmail.com>
2020-10-12 22:48:12 -0500

    finished

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,60 @@
+# Describes an BankAccount
 class BankAccount
+  ### CLASS DEFINITIONS ###
+
+
+  # Class variables
+  @@all = [] # list of all BankAccounts
+
+  # Reader for all class variable
+  def self.all
+    @@all
+  end  
+
+
+  ### INSTANCE DEFINITIONS ###
+
+  # instance readers and writers
+  attr_accessor :status, :balance
+  attr_reader :name 
+
+  # default constructor
+  def initialize(name=nil)
+    @name = name
+    @balance = 1000
+    self.status = "open"
+
+    self.save
+  end
+
+  # saves BankAccount instance to all class variable
+  def save
+    self.class.all << self
+  end
+
+  # Increases the accounts balance based on the deposit ammount
+  def deposit(amount)
+    @balance += amount
+  end
+
+  # Returns a valid sentence reading the user their balance
+  def display_balance
+    p "Your balance is $1000."
+  end
+
+  # Returns whether or not the current account is valid
+  def valid?
+    @balance > 0 && status == "open"
+  end
+
+  # Changes the account status to closed
+  def close_account
+    self.status = "closed"
+  end
+
+  # Decreases the balance based on the amount
+  def withdraw(amount)
+    @balance = @balance - amount
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,50 @@
+# Describes an Transfer
 class Transfer
-  # your code here
+  ### CLASS DEFINITIONS ###
+
+
+
+
+  ### INSTANCE DEFINITIONS ###
+
+  # instance readers and writers
+  attr_accessor :sender, :receiver, :amount, :status
+
+  # default constructor
+  def initialize(sender=nil, receiver=nil, amount=nil)
+    self.sender = sender
+    self.receiver = receiver
+    self.amount = amount
+    self.status = "pending"
+  end
+
+  # Returns whether the transaction is valid
+  def valid?
+    self.receiver.valid? && self.sender.valid?
+  end
+
+  # transfers the amount between accounts if it is a valid transfer
+  def execute_transaction
+    if self.status != "complete" && self.valid? == true && self.sender.balance >= amount
+      self.sender.withdraw(self.amount)
+      self.receiver.deposit(self.amount) 
+      self.status = "complete"
+    else 
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  # transfers the money from the receiver back to the senders account
+  def reverse_transfer
+    if self.status == "complete" && self.valid? == true && self.receiver.balance >= amount
+      self.receiver.withdraw(self.amount)
+      self.sender.deposit(self.amount) 
+      self.status = "reversed"
+    else 
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
 end
